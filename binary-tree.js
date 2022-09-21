a4d34d33ee0fde6bdb24b677bb7224cd5ca5908d
Jordan Wong <pjwong97@gmail.com>
2022-09-21 12:41:45 -0700

    complete main binary tree exercises

--- binary-tree.js
+++ binary-tree.js
@@ -17,21 +17,84 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth(node = this.root) {
+    let toVisit = [this.root];
+    let depthCount = 1;
 
+    if (this.root === null) return 0;
+
+    while (toVisit.length) {
+      let current = toVisit.shift();
+
+      if (current.left === null & current.right === null) {
+        return depthCount;
+      }
+
+      if (current.left !== null) {
+        toVisit.push(current.left);
+      }
+
+      if (current.right !== null) {
+        toVisit.push(current.right);
+      }
+      depthCount++;
+    }
+    return depthCount;
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth(node = this.root) {
+    let toVisit = [this.root];
+    let depthCount = 1;
 
+    if (this.root === null) return 0;
+
+    while (toVisit.length) {
+      let current = toVisit.shift();
+
+      if (current.left !== null &&
+        (current.left.left !== null || current.left.right !== null)) {
+        toVisit.push(current.left);
+      }
+
+      if (current.right !== null &&
+        (current.right.left !== null || current.right.right !== null)) {
+        toVisit.push(current.right);
+      }
+      depthCount++;
+    }
+    return depthCount;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound, node = this.root) {
+    let smallestVal = null;
+    let toVisit = [this.root];
 
+    if (this.root === null) return null;
+
+    while (toVisit.length) {
+      let current = toVisit.shift();
+
+      if (current.val > lowerBound) {
+        if (smallestVal === null) {
+          smallestVal = current.val;
+        } else {
+          smallestVal = smallestVal > current.val ? current.val : smallestVal;
+        }
+      }
+
+      if (current.left !== null) {
+        toVisit.push(current.left);
+      }
+      if (current.right !== null) {
+        toVisit.push(current.right);
+      }
+    }
+    return smallestVal;
   }
 
   /** Further study!
